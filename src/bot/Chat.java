8e6ee811d8d36adb835013bd73aa9494bcfada66
Gisselle Lagos <gisselle.lagos@unitec.edu>
2017-07-11 20:09:54 -0600

    Distance calculation done properly

--- src/bot/Chat.java
+++ src/bot/Chat.java
@@ -5,7 +5,14 @@
  */
 package bot;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.JSONObject;
 
 /**
  *
@@ -20,7 +27,6 @@ public class Chat extends javax.swing.JFrame {
         initComponents();
         this.setSize(660, 460);
         this.setLocationRelativeTo(null);
-        directionMatrix(50);
     }
 
     /**
@@ -72,7 +78,7 @@ public class Chat extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel4, javax.swing.GroupLayout.DEFAULT_SIZE, 640, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -88,79 +94,109 @@ public class Chat extends javax.swing.JFrame {
 
     private void jLabel1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel1MouseClicked
         chat = chatArea.getText();
-   
-        if(userMsj.getText().equals("")){
+
+        if (userMsj.getText().equals("")) {
             System.out.println("Empty message!");
-        }
-        else{
+        } else {
             chatArea.setText(chat.concat(userMsj.getText() + "\n"));
             String response = talkToMe(userMsj.getText());
             userMsj.setText("");
             chat = chatArea.getText();
-            
+
             //puntos y respuesta
-            hiloPuntos = new Hilo(chatArea,response);
+            hiloPuntos = new Hilo(chatArea, response);
             pts = new Thread(hiloPuntos);
             pts.start();
-            
+
         }
-        
-        
-        
+
     }//GEN-LAST:event_jLabel1MouseClicked
 
-    private String talkToMe(String user){
+    private String talkToMe(String user) {
         String response = "";
         user = user.toLowerCase();
         Random r = new Random();
         int num = r.nextInt(3) + 1;
-        
+
         //Greetings
-        if(user.equals("hi") || user.contains("hello") || user.contains("hola")){
+        if (user.equals("hi") || user.contains("hello") || user.contains("hola")) {
             response = "Hello";
         }
-        
-        if(user.contains("my name is") || user.contains("i am") || user.contains("i'm")){
-            if(user.contains("my name is")){
+
+        //presentation
+        if (user.contains("my name is") || user.contains("i am") || user.contains("i'm")) {
+            if (user.contains("my name is")) {
                 userName = user.substring(10);
-            }
-            else if(user.contains("i am")){
+            } else if (user.contains("i am")) {
                 userName = user.substring(4);
-            }
-            else if(user.contains("i'm")){
+            } else if (user.contains("i'm")) {
                 userName = user.substring(3);
             }
-           
+
             response = "Nice to meet you" + userName;
         }
-        
-        if(user.contains("who") && user.contains("you")){
+
+        //my name is korra
+        if (user.contains("who") && user.contains("you")) {
             response = "My name is Korra";
         }
-        System.out.println(response);
+
+        //calculate distance
+        Chat http = new Chat();
+        if (user.contains("distance") && user.contains("and") && user.contains("between")) {
+            System.out.println("Testing 1 - Send Http GET request");
+            String city1 = captureCity1(user);
+            String city2 = captureCity2(user);
+            String km = "";
+            try {
+                km = http.calculateDistance(city1, city2);
+            } catch (Exception ex) {
+                Logger.getLogger(Chat.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            city1 = city1.substring(0, 1).toUpperCase() + city1.substring(1);
+            city2 = city2.substring(0, 1).toUpperCase() + city2.substring(1);
+
+            response = "The distance between " + city1 + " and " + city2 + " is " + km;
+        }
+
         return response;
     }
-    
-    private int[][] directionMatrix(int n){
-        int dir[][] = new int[n][n];
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < 50; j++){
-                if(i >= j)
-                    System.out.print("[ X ]");
-                
-                else
-                    System.out.print("[ 1 ]");
+
+    private String captureCity1(String user) {
+        String city1 = "";
+        int cityStartID = 0, cityFinishID = 0;
+        for (int i = 0; i < user.length() - 1; i++) {
+            System.out.println(user.charAt(i));
+            if (user.charAt(i) == 't' && user.charAt(i + 1) == 'w' && user.charAt(i + 2) == 'e' && user.charAt(i + 3) == 'e' && user.charAt(i + 4) == 'n') {
+                cityStartID = i + 6;
+            } else if (user.charAt(i) == 'a' && user.charAt(i + 1) == 'n' && user.charAt(i + 2) == 'd') {
+                cityFinishID = i - 1;
             }
-            
-            System.out.println("");
         }
-        return dir;
+
+        city1 = user.substring(cityStartID, cityFinishID);
+        return city1;
     }
+
+    private String captureCity2(String user) {
+        String city2 = "";
+        int cityStartID = 0, cityFinishID = user.length();
+        for (int i = 0; i < user.length() - 1; i++) {
+            if (user.charAt(i) == 'a' && user.charAt(i + 1) == 'n' && user.charAt(i + 2) == 'd' && user.charAt(i + 3) == ' ') {
+                cityStartID = i + 4;
+            }
+        }
+        city2 = user.substring(cityStartID, cityFinishID);
+        return city2;
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
+
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
@@ -190,6 +226,53 @@ public class Chat extends javax.swing.JFrame {
                 new Chat().setVisible(true);
             }
         });
+
+    }
+
+    public String calculateDistance(String city1, String city2) throws Exception {
+
+        String origin = city1.replace(' ', '+'), destination = city2.replace(' ', '+');
+        String YOUR_API_KEY = "AIzaSyCm5bDkt7Ohgm4l5VhBstydD-fGSmcr6oo";
+        String url = "https://maps.googleapis.com/maps/api/directions/json?origin="
+                + origin + "&destination=" + destination + "&key=" + YOUR_API_KEY;
+
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        // optional default is GET
+        con.setRequestMethod("GET");
+
+        //add request header
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        System.out.println(response.toString());
+        String routeDistance = "";
+        try {
+            String jsonStringDistance = response.toString();
+            System.out.println(jsonStringDistance);
+            JSONObject jsonObjDistance = new JSONObject(jsonStringDistance);
+            routeDistance = jsonObjDistance.getJSONArray("routes").getJSONObject(0).getJSONArray("legs").getJSONObject(0).getJSONObject("distance").get("text").toString();
+            System.out.println(routeDistance);
+        } catch (Exception ex) {
+            Logger.getLogger(Chat.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return routeDistance;
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -199,10 +282,11 @@ public class Chat extends javax.swing.JFrame {
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextField userMsj;
     // End of variables declaration//GEN-END:variables
-    String chat= "";
+    String chat = "";
     String userName = "";
     public Hilo hiloPuntos;
     Thread pts;
+    private final String USER_AGENT = "Mozilla/5.0";
+    private String distance = "";
+
 }
-
-
